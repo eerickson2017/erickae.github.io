30db47d8f2bd5dd557b9e17ab2de9fad8a2094d8
eerickson2017 <144744996+eerickson2017@users.noreply.github.com>
2024-02-22 15:24:01 -0800

    small textual edits

--- ecomerce-project/js/script.js
+++ ecomerce-project/js/script.js
@@ -1,12 +1,13 @@
+
 const bar = document.getElementById('bar');
 const nav = document.getElementById('navbar');
 const close = document.getElementById('close');
 const star = document.getElementById('star');
 const pro = document.getElementById('pro');
-const proContainer = document.getElementById('pro-container');
 const features = document.getElementById('feature');
 const newArrivals = document.getElementById('pro');
 let html = '';
+
 
 
 if (bar){
@@ -19,7 +20,23 @@ if (close){
     nav.classList.remove('active');
     })
 }
-
+ 
+features.addEventListener('click',(e)=> {
+    if(e.target.tagName === 'IMG' || e.target.tagName === 'H6'){
+        console.log(e.tagName);
+        const div = document.createElement('div');
+        div.className ='fe-box';
+        const img = document.createElement('img');
+        const h6 = document.createElement('h6');
+        h6.textContent = 'support';
+        img.src ='imgs/f3.png';
+        div.appendChild(img);
+        div.appendChild(h6);
+        features.appendChild(div);
+        console.log(features);
+    
+    }
+})
 
 
 // addcart
@@ -29,33 +46,40 @@ var addcart = document.getElementsByClassName('addcart')
 
 console.log(addcart);
 
-//Product desc loop function 
+// //Product desc loop function 
+// const proContainer = document.getElementById('pro-Container');
 
-function productdesc(){
-    let html = '';
-    for (let i = 0; i <= 8; i++){
-        html += `<div class="pro">
-        <div id="pro">
-           ${getRandomPicture()}
-        </div>
-        <div class="desc">
-            <span>Adidas</span>
-            <h5>Cartoon Astronaut T-shirt</h5>
-            <div class="star">
-                <i class="fas fa-star"></i>
-                <i class="fas fa-star"></i>
-                <i class="fas fa-star"></i>
-                <i class="fas fa-star"></i>
-                <i class="fas fa-star"></i>
-            </div>
-            <h4>$74</h4>
-        </div>
-        <a href="#"><i class="fa-solid fa-cart-shopping cart"></i></a>
-    </div>`;
-    }
-    return html;
-}
-proContainer.innerHTML = productdesc();
+// function productdesc(){
+//     let html = '';
+//     for (let i = 0; i <= 8; i++){
+//         html += `<div class="pro">
+//         <div id="pro">
+//            ${getRandomPicture()}
+//         </div>
+//         <div class="desc">
+//             <span>Adidas</span>
+//             <h5>Cartoon Astronaut T-shirt</h5>
+//             <div class="star">
+//                 <i class="fas fa-star"></i>
+//                 <i class="fas fa-star"></i>
+//                 <i class="fas fa-star"></i>
+//                 <i class="fas fa-star"></i>
+//                 <i class="fas fa-star"></i>
+//             </div>
+//             <h4>$74</h4>
+//         </div>
+//         <button ><i class="fa-solid fa-cart-shopping cart"></i></button>
+//     </div>`;
+//     }
+    
+//     return html;
+// }
+// proContainer.innerHTML = productdesc();
+
+let btn = document.getElementsByClassName('remove');
+let cartItemsList = document.getElementById('cartItems');
+
+
 
 //generate randompicture function
 
@@ -65,42 +89,42 @@ proContainer.innerHTML = productdesc();
         return html += `<img src="imgs/products/f${randomNum}.jpg" alt=""></img>`;
     }
 
-    //featurebox loop
+// //featurebox loop
     
-    let icons = '';
-    for(let key in feature){
+//     let icons = '';
+//     for(let key in feature){
         
-        icons += `<div class="fe-box">
-        <img src=${feature[key].img} alt="">
-        <h6>${feature[key].name}</h6>
-        </div>`;
-    }
-    features.innerHTML = icons;
+//         icons += `<div class="fe-box">
+//         <img src=${feature[key].img} alt="">
+//         <h6>${feature[key].name}</h6>
+//         </div>`;
+//     }
+//     features.innerHTML = icons;
 
-    //New Arrivals loop
+//New Arrivals loop
 
-    let descCard ='';
+    // let descCard ='';
 
-    for(let key in newArrival){
-        descCard += `<div class="pro">
-        <img src=${newArrival[key].img} alt="">
-        <div class="desc">
-            <span>Adidas</span>
-            <h5>Cartoon Astronaut T-shirt</h5>
-            <div class="star">
-                <i class="fas fa-star"></i>
-                <i class="fas fa-star"></i>
-                <i class="fas fa-star"></i>
-                <i class="fas fa-star"></i>
-                <i class="fas fa-star"></i>
-            </div>
-            <h4>$ ${newArrival[key].price}</h4>
-        </div>
-        <a href="#"><i class="fa-solid fa-cart-shopping cart"></i></a>
-    </div>`;
-    }
+    // for(let key in newArrival){
+    //     descCard += `<div class="pro">
+    //     <img src=${newArrival[key].img} alt="">
+    //     <div class="desc">
+    //         <span>Adidas</span>
+    //         <h5>Cartoon Astronaut T-shirt</h5>
+    //         <div class="star">
+    //             <i class="fas fa-star"></i>
+    //             <i class="fas fa-star"></i>
+    //             <i class="fas fa-star"></i>
+    //             <i class="fas fa-star"></i>
+    //             <i class="fas fa-star"></i>
+    //         </div>
+    //         <h4>$ ${newArrival[key].price}</h4>
+    //     </div>
+    //     <a href="#"><i class="fa-solid fa-cart-shopping cart"></i></a>
+    // </div>`;
+    // }
 
-    newArrivals.innerHTML = descCard;
+    // newArrivals.innerHTML = descCard;
 
     
 
